9965b0e39db02e60c292920d9f4d28aaa7201f66
DARIN NELSON <nelson.darinp@gmail.com>
2020-04-04 09:35:42 -0500

    Courses added

---
+++ db/seeds.rb
@@ -0,0 +1,30 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Course.create(name: "Mexico", image_url: "https://flagpedia.net/data/flags/w580/mx.png")
+Course.create(name: "Spain", image_url: "https://flagpedia.net/data/flags/w580/es.png")
+Course.create(name: "Costa Rica", image_url: "https://flagpedia.net/data/flags/w580/cr.png")
+Course.create(name: "Guatemala", image_url: "https://flagpedia.net/data/flags/w580/gt.png")
+Course.create(name: "Honduras", image_url: "https://flagpedia.net/data/flags/w580/hn.png")
+Course.create(name: "Nicaragua", image_url: "https://flagpedia.net/data/flags/w580/ni.png")
+Course.create(name: "Panama", image_url: "https://flagpedia.net/data/flags/w580/pa.png")
+Course.create(name: "Cuba", image_url: "https://flagpedia.net/data/flags/w580/cu.png")
+Course.create(name: "El Salvador", image_url: "https://flagpedia.net/data/flags/w580/sv.png")
+Course.create(name: "Dominican Republic", image_url: "https://flagpedia.net/data/flags/w580/do.png")
+Course.create(name: "Puerto Rico", image_url: "https://flagpedia.net/data/flags/w580/pr.png")
+Course.create(name: "Argentina", image_url: "https://flagpedia.net/data/flags/w580/ar.png")
+Course.create(name: "Bolivia", image_url: "https://flagpedia.net/data/flags/w580/bo.png")
+Course.create(name: "Chile", image_url: "https://flagpedia.net/data/flags/w580/cl.png")
+Course.create(name: "Colombia", image_url: "https://flagpedia.net/data/flags/w580/co.png")
+Course.create(name: "Ecuador", image_url: "https://flagpedia.net/data/flags/w580/ec.png")
+Course.create(name: "Paraguay", image_url: "https://flagpedia.net/data/flags/w580/py.png")
+Course.create(name: "Peru", image_url: "https://flagpedia.net/data/flags/w580/pe.png")
+Course.create(name: "Uruguay", image_url: "https://flagpedia.net/data/flags/w580/uy.png")
+Course.create(name: "Venezuela", image_url: "https://flagpedia.net/data/flags/w580/ve.png")
+Course.create(name: "Equatorial Guinea", image_url: "https://flagpedia.net/data/flags/w580/gq.png")
+Course.create(name: "Brazil", image_url: "https://flagpedia.net/data/flags/w580/br.png")
