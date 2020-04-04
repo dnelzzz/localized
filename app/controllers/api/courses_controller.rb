6dc76eca8f1989764e61b6cb9195069a52043a12
DARIN NELSON <nelson.darinp@gmail.com>
2020-04-04 10:09:41 -0500

    Add new course

--- app/controllers/api/courses_controller.rb
+++ app/controllers/api/courses_controller.rb
@@ -4,6 +4,15 @@ class Api::CoursesController < ApplicationController
     render "index.json.jbuilder"
   end
 
+  def create
+    @course = Course.new(
+      name: params[:name],
+      image_url: params[:image_url]
+    )
+    @course.save
+    render "show.json.jbuilder"
+  end
+
   def show
     @course = Course.find_by(name: params[:name])
     render "show.json.jbuilder"
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     get "/courses" => "courses#index"
+    post "/courses" => "courses#create"
     get "/courses/:name" => "courses#show"
+    patch "/courses/:name" => "courses#update"
   end
 end
